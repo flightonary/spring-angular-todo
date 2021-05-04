83f10c57b040a0598f4e2cf63a4fa9ce2e8460f8
tonary <nekomelife@gmail.com>
2021-05-05 08:48:00 +0900

    Install barista

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { DtIconModule } from '@dynatrace/barista-components/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        DtIconModule.forRoot({ svgIconLocation: '/assets/icons/{{name}}.svg' }),
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
